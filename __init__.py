7db6bf25b668241833a69eebd41c34e533ef2f10
Ren Tatsumoto <tatsu@autistici.org>
2020-09-04 01:27:47 +0300

    fix idea complaints

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from anki.notes import Note
 from aqt import mw
 from anki.lang import _
 from aqt import gui_hooks
@@ -5,7 +6,7 @@ from aqt.utils import tooltip
 from aqt.browser import Browser
 
 
-def addSecondToFirst(note1, note2):
+def addSecondToFirst(note1: Note, note2: Note) -> None:
     for (name, value) in note2.items():
         # don't wast cycles on empty fields
         if value and name in note1:
@@ -15,17 +16,17 @@ def addSecondToFirst(note1, note2):
 
 
 # Col is a collection of cards, cids are the ids of the cards to merge
-def mergeSelectedCardFields(cids):
+def mergeSelectedCardFields(cids: list) -> None:
     cards = [mw.col.getCard(cid) for cid in cids]
-    cards = sorted(cards, key = lambda card: card.due)
+    cards = sorted(cards, key=lambda card: card.due)
     notes = [card.note() for card in cards]
 
     # Iterate till 1st element and keep on decrementing i
     for i in reversed(range(len(cids) - 1)):
-        addSecondToFirst(notes[i], notes[i+1])
+        addSecondToFirst(notes[i], notes[i + 1])
 
 
-def onBrowserMergeCards(self):
+def onBrowserMergeCards(self) -> None:
     cids = self.selectedCards()
 
     if len(cids) < 2:
@@ -40,10 +41,10 @@ def onBrowserMergeCards(self):
     self.model.endReset()
     self.mw.reset()
 
-    tooltip("{} cards merged.".format(len(cids)), parent=self)
+    tooltip(f"{len(cids)} cards merged.", parent=self)
 
 
-def onBrowserSetupMenus(self):
+def onBrowserSetupMenus(self) -> None:
     menu = self.form.menu_Cards
     a = menu.addAction("Merge fields")
     a.triggered.connect(self.onBrowserMergeCards)
