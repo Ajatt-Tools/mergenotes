198adeccfb480148ae418c9f59f6b9bcfc15e06c
Ren Tatsumoto <tatsu@autistici.org>
2020-12-08 03:13:00 +0300

    add UI

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from anki.notes import Note
 from aqt import mw
 from anki.lang import _
-from aqt import gui_hooks, QKeySequence
+from aqt import gui_hooks
+from aqt.qt import *
 from aqt.utils import tooltip
 from aqt.browser import Browser
 
@@ -76,25 +77,116 @@ def onBrowserMergeCards(self) -> None:
     tooltip(f"{len(cids)} cards merged.", parent=self)
 
 
-def onBrowserSetupMenus(self) -> None:
-    menu = self.form.menu_Cards
-    a = menu.addAction("Merge fields")
-    a.setShortcut(QKeySequence(config['shortcut']))
-    a.triggered.connect(self.onBrowserMergeCards)
+def onMergeFieldsSettings() -> None:
+    dialog = MergeFieldsSettingsWindow()
+    dialog.exec_()
+
 
 ######################################################################
 # UI Layout
 ######################################################################
+
+class DialogUI(QDialog):
+    def __init__(self):
+        QDialog.__init__(self, parent=mw)
+        self.fieldSeparatorLineEdit = QLineEdit()
+        self.deleteOriginalNotesCheckBox = QCheckBox("Delete original notes")
+        self.mergeTagsCheckBox = QCheckBox("Merge tags")
+        self.reverseOrderCheckBox = QCheckBox("Reverse order")
+        self.okButton = QPushButton("Ok")
+        self.cancelButton = QPushButton("Cancel")
+        self._setupUI()
+
+    def _setupUI(self):
+        self.setWindowTitle('Merge Fields Settings')
+        self.setLayout(self.setupOuterLayout())
+        self.addToolTips()
+
+    def setupOuterLayout(self):
+        vbox = QVBoxLayout()
+        vbox.setSpacing(10)
+        vbox.addLayout(self.createFieldSeparatorInputBox())
+        vbox.addLayout(self.createCheckBoxGroup())
+        vbox.addStretch(1)
+        vbox.addLayout(self.createBottomGroup())
+        return vbox
+
+    def createFieldSeparatorInputBox(self):
+        hbox = QHBoxLayout()
+        hbox.addWidget(QLabel("Field Separator:"))
+        hbox.addWidget(self.fieldSeparatorLineEdit)
+        return hbox
+
+    def createCheckBoxGroup(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.deleteOriginalNotesCheckBox)
+        vbox.addWidget(self.mergeTagsCheckBox)
+        vbox.addWidget(self.reverseOrderCheckBox)
+        return vbox
+
+    def createBottomGroup(self):
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.okButton)
+        hbox.addWidget(self.cancelButton)
+        hbox.addStretch()
+        return hbox
+
+    def addToolTips(self):
+        pass  # TODO
+        # .setToolTip()
 
 
 ######################################################################
 # Settings window
 ######################################################################
 
+class MergeFieldsSettingsWindow(DialogUI):
+    def __init__(self):
+        super().__init__()
+        self.setDefaultValues()
+        self.connectUIElements()
+
+    def setDefaultValues(self):
+        self.fieldSeparatorLineEdit.setText(config['field_separator'])
+        self.deleteOriginalNotesCheckBox.setChecked(config['delete_original_notes'])
+        self.mergeTagsCheckBox.setChecked(config['merge_tags'])
+        self.reverseOrderCheckBox.setChecked(config['reverse_order'])
+
+    def connectUIElements(self):
+        self.okButton.clicked.connect(self.onConfirm)
+        self.cancelButton.clicked.connect(self.close)
+
+    def onConfirm(self):
+        config['field_separator']: str = self.fieldSeparatorLineEdit.text()
+        config['delete_original_notes']: bool = self.deleteOriginalNotesCheckBox.isChecked()
+        config['merge_tags']: bool = self.mergeTagsCheckBox.isChecked()
+        config['reverse_order']: bool = self.reverseOrderCheckBox.isChecked()
+        mw.addonManager.writeConfig(__name__, config)
+        self.close()
+
 
 ######################################################################
 # Entry point
 ######################################################################
+
+
+def setupContextMenu(browser: Browser) -> None:
+    menu = browser.form.menu_Cards
+    merge_fields_action = menu.addAction("Merge fields")
+    merge_fields_action.setShortcut(QKeySequence(config['shortcut']))
+    merge_fields_action.triggered.connect(browser.onBrowserMergeCards)
+
+
+def setupEditMenu(browser: Browser) -> None:
+    edit_menu = browser.form.menuEdit
+    edit_menu.addSeparator()
+    merge_fields_settings_action = edit_menu.addAction('Merge Fields Settings...')
+    merge_fields_settings_action.triggered.connect(onMergeFieldsSettings)
+
+
+def onBrowserSetupMenus(browser: Browser) -> None:
+    setupContextMenu(browser)
+    setupEditMenu(browser)
 
 
 config = getConfig()
