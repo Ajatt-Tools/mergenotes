5f1dd3f3f43b4a806b8cbb9a939f764defca9bcc
Ren Tatsumoto <tatsu@autistici.org>
2021-10-03 01:23:36 +0300

    update packaging script

--- package.sh
+++ package.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env sh
-zip -r mergenotes ./*.py ./config.*
+
+readonly ADDON_NAME=mergenotes
+readonly ROOT_DIR=$(git rev-parse --show-toplevel)
+readonly BRANCH=${1:-$(git branch --show-current)}
+readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
+
+cd -- "$ROOT_DIR" || exit 1
+
+git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
