c7ace9452d6d6a4cc13aec1bda859bab1c47d8c5
Ren Tatsumoto <tatsu@autistici.org>
2021-11-18 00:20:01 +0300

    package ajt common with the addon

--- package.sh
+++ package.sh
@@ -6,5 +6,13 @@ readonly BRANCH=${1:-$(git branch --show-current)}
 readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
 
 cd -- "$ROOT_DIR" || exit 1
+export ROOT_DIR
 
 git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
+
+# shellcheck disable=SC2016
+git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}.zip"'
+
+zipmerge "$ZIP_NAME" ./*.zip
+
+rm -- ./*.zip
