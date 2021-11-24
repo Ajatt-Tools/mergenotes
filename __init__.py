ea21017535cbd40263b2469722e91f3e8d2046bc
Ren Tatsumoto <tatsu@autistici.org>
2021-11-24 21:51:10 +0300

    add copyrights

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+# Copyright: Ren Tatsumoto <tatsu at autistici.org>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 from . import merge_duplicates, merge_notes, settings_dialog
 
 merge_notes.init()
--- settings_dialog.py
+++ settings_dialog.py
@@ -1,3 +1,6 @@
+# Copyright: Ren Tatsumoto <tatsu at autistici.org>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 from typing import Iterable, Tuple
 
 from aqt import mw, gui_hooks
