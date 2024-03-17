c002927c03e1a9f239cdb681e3a1de2e9d42e874
Ren Tatsumoto <tatsu@autistici.org>
2024-03-17 23:26:34 +0300

    update package script

--- package.sh
+++ package.sh
@@ -1,18 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-readonly ADDON_NAME=mergenotes
-readonly ROOT_DIR=$(git rev-parse --show-toplevel)
-readonly BRANCH=${1:-$(git branch --show-current)}
-readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
-
-cd -- "$ROOT_DIR" || exit 1
-export ROOT_DIR
-rm -- "$ZIP_NAME" 2>/dev/null || true
-git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
-
-# shellcheck disable=SC2016
-git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}.zip"'
-
-zipmerge "$ZIP_NAME" ./*.zip
-
-rm -- ./*.zip
+./ajt_common/package.sh \
+	--package "MergeNotes" \
+	--name "AJT Merge Notes" \
+	"$@"
