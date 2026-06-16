75848af86ab2190979848082f2236e77bcb7aa5e
Ren Tatsumoto <tatsu@autistici.org>
2026-06-16 14:42:42 +0300

    refactor init

--- merge_notes/__init__.py
+++ merge_notes/__init__.py
@@ -1,10 +1,20 @@
-# Copyright: Ren Tatsumoto <tatsu at autistici.org>
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-from . import duplicate_notes, find_duplicates, merge_duplicates, merge_notes, settings_dialog
+import sys
 
-merge_notes.init()
-merge_duplicates.init()
-settings_dialog.init()
-duplicate_notes.init()
-find_duplicates.init()
+from aqt import mw
+
+
+def start_addon() -> None:
+    from . import duplicate_notes, find_duplicates, merge_duplicates, merge_notes, settings_dialog
+
+    merge_notes.init()
+    merge_duplicates.init()
+    settings_dialog.init()
+    duplicate_notes.init()
+    find_duplicates.init()
+
+
+if mw and "pytest" not in sys.modules:
+    start_addon()
