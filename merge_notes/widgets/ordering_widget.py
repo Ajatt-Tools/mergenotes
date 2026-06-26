1b32fae5c3552a7a87255b913077c6f84a995f08
Ren Tatsumoto <tatsu@autistici.org>
2026-06-26 04:29:02 +0300

    remove unused import

--- merge_notes/widgets/ordering_widget.py
+++ merge_notes/widgets/ordering_widget.py
@@ -1,6 +1,5 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-from collections.abc import Iterable
 from typing import Optional
 
 from aqt.qt import *
